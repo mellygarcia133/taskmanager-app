659b0c031f917163fae77024e2037e42a6aab7da
Melissa Garcia <mellygarcia133@users.noreply.github.com>
2021-01-26 11:05:51 -0800

    modifications to pages, readme, and deploy to gh pages

--- taskmanager-app/src/context/TaskListContext.js
+++ taskmanager-app/src/context/TaskListContext.js
@@ -1,5 +1,5 @@
 import React, {createContext, useState, useEffect} from 'react';
-import { v1 as uuid } from 'uuid';
+import { uuid } from 'uuidv4';
 
 export const TaskListContext = createContext();
 
