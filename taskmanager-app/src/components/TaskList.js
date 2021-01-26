82867a453b3ff6e0b0fb34daaca5fd3642e63fae
Melissa Garcia <mellygarcia133@users.noreply.github.com>
2021-01-25 16:36:57 -0800

    edit/add task on task form and tasklist mapping

--- taskmanager-app/src/components/TaskList.js
+++ taskmanager-app/src/components/TaskList.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import Task from '../components/Task';
+import React, {useContext} from 'react';
+import {TaskListContext} from '../context/TaskListContext';
+import Task from "../components/Task";
 
 const TaskList = () => {
+    const { tasks } = useContext(TaskListContext);
     return (
         <div>
-            <ul className="list">
-
-            </ul>
+            {tasks.length ? (
+                <ul className="list">
+                    {tasks.map(task => {
+                        return <Task task={task} key={task.id} />;
+                    })}
+                </ul>
+            ) : (
+                <div className="no-tasks">No Tasks</div>
+            )}
         </div>
     );
 };
